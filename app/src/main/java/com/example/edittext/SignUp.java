bdb63d63cda431b51112dc76264f6462ffcfcf34
Chirag Arora <chirag.arora2015@vit.ac.in>
2017-03-08 01:18:26 +0530

    Username & Password added.

--- app/src/main/java/com/example/edittext/SignUp.java
+++ app/src/main/java/com/example/edittext/SignUp.java
@@ -5,15 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
-    public void Click(View v){
-        startActivity(new Intent(MainActivity.this,SignUp.class));
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 }
